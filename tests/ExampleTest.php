0ee7c5ab7d521d71085c2891905eaf3415ab14ba
Dobroslav Tokoš <dobroslav@tokos.sk>
2023-02-02 14:10:40 +0100

    Installed phpunit/phpunit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase {
+    public function testExample(): void {
+        $this->assertEquals(2, 1 + 1);
+    }
+}
